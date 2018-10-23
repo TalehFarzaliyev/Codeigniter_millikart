df9ab3726ffbce8cbe470eeac67a2d0932f03e49
TalehFarzaliyev <tferzeliyev@std.qu.edu.az>
2018-10-23 12:27:27 +0400

    Update millikart.php

--- millikart.php
+++ millikart.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['millikart_mid'] 			= 'chargepal';
-$config['millikart_secret_key'] 	= '123456qwerty';
+$config['millikart_mid'] 			  = '';
+$config['millikart_secret_key'] = '';
 $config['millikart_status'] 		= '0'; //if change production mode change value to 1
 $config['millikart_currency']		= '944'; //AZN
 $config['millikart_test_url']		= 'http://test.millikart.az:8513';
